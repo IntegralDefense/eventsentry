0ca627cea183c3a8000c7b61f6e6b51955574da1
Matthew Wilson <automationator@runbox.com>
2018-09-13 08:39:30 -0400

    Adds a phishme detection module

---
+++ lib/modules/detections/phishme.py
@@ -0,0 +1,15 @@
+from lib.modules.DetectionModule import *
+
+class Module(DetectionModule):
+    def __init__(self, name, event_json):
+
+        super().__init__(name=name, event_json=event_json)
+
+    def run(self):
+        self.logger.debug('Running the {} detection module'.format(self.name))
+
+        for alert in self.event_json['ace_alerts']:
+            if '[POTENTIAL PHISH]' in alert['description']:
+                self.detections.append('Detected a user-reported phish: {}'.format(alert['description']))
+                self.tags.append('phishme')
+
